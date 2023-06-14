736471a48997bfe919d89835b080a0670b142285
QuackOri <wn0701@naver.com>
2023-06-14 14:10:39 +0900

    second commit, add tcp

--- main.py
+++ main.py
@@ -11,40 +11,53 @@ data = f.read_file('syn_attack.pcap')
 ##### skip global header
 g_payload = p.skip(data)
 n_payload = g_payload
+
+index = 0
 while True:
-    # input()
+    index += 1
     if len(n_payload) < 16:
         break
+    print()
 
     ##### parse packet header
+    print("########## {} Packet Header ##########".format(index))
     p_header, p_payload = pkt.parse(n_payload)
     n_payload = p_payload[p_header.orig_len:]
     p_payload = p_payload[:p_header.orig_len]
-    # print(p_header)
+    print(p_header)
     # print(p_payload)
 
     ##### parse ethernet header
+    print("########## Ethernet ##########")
     e_header, e_payload = e.parse(p_payload)
-    # print(e_header)
+    print(e_header)
     ##### check ipv4
     if not f.is_ipv4(e_header):
         continue
     
     ##### parse ip header
+    print("########## IP Header ##########")
     i_header, i_payload = i.parse(e_payload)
-    # print(i_header)
+    print(i_header)
 
     ##### check UDP
-    if not f.which_protocol(i_header) == "UDP":
-        continue
-    
-    ##### parse udp header
-    u_header, u_payload = t.udp_parse(i_payload)
-    # print(u_header)
-    ##### only DNS Response
-    if not u_header.src_port == 53:
-        continue
+    if f.which_protocol(i_header) == "UDP":
+        ##### parse udp header
+        print("########## UDP Header ##########")
+        u_header, u_payload = t.udp_parse(i_payload)
 
-    ##### parse DNS header
-    d_header, d_payload = d.parse(u_payload)
-    f.print_answer_ip(d_header.answers)
+        ##### only DNS Response
+        if not u_header.src_port == 53:
+            continue
+
+        ##### parse DNS header
+        print("########## DNS Header ##########")
+        d_header, d_payload = d.parse(u_payload)
+        print(d_header)
+        # f.print_answer_ip(d_header.answers)
+
+    elif f.which_protocol(i_header) == "TCP":
+        ##### parse tcp header
+        print("########## TCP Header ##########")
+        t_header, t_payload = t.tcp_parse(i_payload)
+        print(t_header)
--- transport.py
+++ transport.py
@@ -1,7 +1,19 @@
 from dataclasses import dataclass
 from struct import *
 
-header_size = 2 + 2 + 2 + 2
+udp_header_size = 2 + 2 + 2 + 2
+tcp_header_size = 2 + 2 + 4 + 4 + 2 + 2 + 2 + 2
+
+@dataclass
+class TCP:
+    src_port: int
+    dst_port: int
+    sequence_number: int
+    acknowledgment_number: int
+    header_length_and_flags: bytes
+    window: int
+    checksum: bytes
+    urgent_pointer: int
 
 @dataclass
 class UDP:
@@ -13,12 +25,19 @@ class UDP:
 
     # return header length
     def __len__(self):
-        return header_size
+        return udp_header_size
     
 def udp_parse(data):
-    header = data[:header_size]
-    payload = data[header_size:]
+    header = data[:udp_header_size]
+    payload = data[udp_header_size:]
     # print(header)
     src_port, dst_port, length, checksum = unpack('>HHHH', header)
 
     return UDP(src_port, dst_port, length, checksum), payload
+
+def tcp_parse(data):
+    header = data[:tcp_header_size]
+    payload = data[tcp_header_size:]
+    src_port, dst_port, s_number, ack_number, HaF, window, checksum, urgent_pointer = unpack("HHIIHHHH", header)
+
+    return TCP(src_port, dst_port, s_number, ack_number, HaF, window, checksum, urgent_pointer), payload
