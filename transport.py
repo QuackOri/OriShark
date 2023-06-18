36bf3a84322de54f3558d4210a0a335e6502ce8e
이종학 <52825485+P4P3R-HAK@users.noreply.github.com>
2023-06-18 16:49:38 +0900

    Update transport.py
    back

--- transport.py
+++ transport.py
@@ -8,15 +8,14 @@ tcp_header_size = 2 + 2 + 4 + 4 + 2 + 2 + 2 + 2
 class TCP:
     src_port: int
     dst_port: int
-    sequence_number: bytes
-    acknowledgment_number: bytes
+    sequence_number: int
+    acknowledgment_number: int
     header_length: int
-    flags: str
-    checksum: int
+    flags: list[str]
+    window: int
+    checksum: bytes
     urgent_pointer: int
-    tcp_payload: bytes
 
-    # return header length
     def __len__(self):
         return tcp_header_size
 
@@ -59,39 +58,10 @@ def divide_headerL_and_flags(headerL_and_flags):
     flags_list = analysis_tcp_flags(flags)
     return int(header_length, 2), flags_list
 
-def flags_parser(flags_bit: str) -> str:
-    flags = ""
-    if flags_bit[0] == "1":
-        flags += "u"
-    if flags_bit[1] == "1":
-        flags += "a"
-    if flags_bit[2] == "1":
-        flags += "p"
-    if flags_bit[3] == "1":
-        flags += "r"
-    if flags_bit[4] == "1":
-        flags += "s"
-    if flags_bit[5] == "1":
-        flags += "f"
-    return flags
-
 def tcp_parse(data):
-    src_port = unpack(">H", data[:2])[0]
-    dst_port = unpack(">H", data[2:4])[0]
-    seq_number = data[4:8]
-    ack_number = data[8:12]
-    header = protocol = unpack("<B", data[12:13])[0]
-    header_length = int(header / 4)
-    flags = unpack("<H", data[13:15])[0]
-    flags_bit = format(flags, 'b')[-6:]
-    flags = flags_parser(flags_bit)
-    checksum = unpack("<H", data[15:17])[0]
-    
-    try:
-        urgent_pointer = unpack("<H", data[17:19])[0]
-    except:
-        urgent_pointer = 0
-    
-    tcp_payload = data[tcp_header_size:]
-    
-    return TCP(src_port, dst_port, seq_number, ack_number, header_length, flags, checksum, urgent_pointer, tcp_payload), tcp_payload
+    header = data[:tcp_header_size]
+    payload = data[tcp_header_size:]
+    src_port, dst_port, s_number, ack_number, HaF, window, checksum, urgent_pointer = unpack(">HHIIHHHH", header)
+    header_length, flags = divide_headerL_and_flags(HaF)
+
+    return TCP(src_port, dst_port, s_number, ack_number, header_length, flags, window, checksum, urgent_pointer), payload
